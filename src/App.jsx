935c3d8f879d9448ccd517451cfe0ec8605e7b46
iwl-lyam <64089164+iwl-lyam@users.noreply.github.com>
2023-08-27 08:37:37 +0000

    things things

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
 import './App.css'
 import Nav from './Nav'
-
+import { BrowserRouter, Routes, Route  } from "react-router-dom";
+import Contact from './Contact'
+import NoPage from './NoPage'
+import Verification from './Verification'
+import Home from './Home'
 
 export default function App() {
   return (
     <div>
-      React ⚛️ + Vite ⚡ + Replit 🌀
+      <Nav />
+      <BrowserRouter>
+        <Routes>
+            <Route index element={<Home />} />
+            <Route path="/verification" element={<Verification />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/*" element={<NoPage />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
---
+++ src/Verification.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Verification() {
+  return (
+    <div>
+      <h1>Verification</h1>
+    </div>
+  )
+}
