f6052144cccbb295540bb738436d8e28929da8f2
itwithlyam <lyam.mosnier@itwithlyam.co.uk>
2023-10-15 16:11:41 +0100

    Start project leader dashboard

--- src/Dashboards/DashboardEntry.jsx
+++ src/Dashboards/DashboardEntry.jsx
@@ -50,11 +50,19 @@ export default function DashboardEntry() {
 
                         <div className="card bg-warning mb-3" style={{width: "18rem"}}>
                     <div className="card-body">
-                        <h5 className="card-title">Executive dashboard</h5>
+                        <h5 className="card-title">Project leader dashboard</h5>
                         <h6 className="card-subtitle mb-2">Permission level 4</h6>
-                        <a href="/dashboards/admin" className="card-link">Open dashboard</a>
+                        <a href="/dashboards/pl" className="card-link">Open dashboard</a>
                     </div>
                 </div>
+
+                        <div className="card bg-warning mb-3" style={{width: "18rem"}}>
+                            <div className="card-body">
+                                <h5 className="card-title">Executive dashboard</h5>
+                                <h6 className="card-subtitle mb-2">Permission level 5</h6>
+                                <a href="/dashboards/admin" className="card-link">Open dashboard</a>
+                            </div>
+                        </div>
             </div>
                 </div>)}
         </div>
--- src/Dashboards/ProjectLeaderDashboard.jsx
+++ src/Dashboards/ProjectLeaderDashboard.jsx
@@ -2,13 +2,13 @@ import react, {useState, useEffect} from 'react'
 import Login from '../Login/MainForm.jsx'
 import Markdown from "react-markdown";
 
-export default function UserDashboard() {
+export default function ProjectLeaderDashboard() {
     const [posts, setPosts] = useState([])
     const [notifs, setNotifs] = useState([])
 
     useEffect(() => {
         const f = async () => {
-            const req = await fetch("http://localhost:8080/api/suggestions?user=1", {
+            const req = await fetch("http://localhost:8080/api/suggestions?status=0", {
                 headers: {
                     Authorization: sessionStorage.token || localStorage.token
                 }
@@ -31,7 +31,7 @@ export default function UserDashboard() {
             <button data-toggle="modal" data-target={`#modal-${post._id}`} type="button"
                     className="btn btn-light mx-auto border border-dark pt-2 m-2 text-center w-100">
                 <h3>{post.title}</h3>
-                <p className="mb-1">Status: {post.status}</p>
+                <p className="mb-1">{post.department}</p>
             </button>
 
             <div className="modal fade" id={`modal-${post._id}`} role="dialog">
@@ -43,9 +43,18 @@ export default function UserDashboard() {
                         </div>
                         <div className="modal-body">
                             <Markdown>{post.description}</Markdown>
-                            <p><strong>Status: {post.status}</strong></p>
                             <p><strong>Department: {post.department}</strong></p>
                             <p><strong>Post ID: {post._id}</strong></p>
+                            <div className="btn-group">
+                                <button className="btn btn-success" onClick={() => {
+                                    // TODO code the approval thing
+                                }}>Approve
+                                </button>
+                                <button className="btn btn-danger" onClick={() => {
+                                    // TODO code the deny thing
+                                }}>Deny
+                                </button>
+                            </div>
                         </div>
                         <div className="modal-footer">
                             <button type="button" className="btn btn-default" data-dismiss="modal">Close</button>
@@ -62,20 +71,20 @@ export default function UserDashboard() {
         <div>
             {!localStorage.token && !sessionStorage.token ? <Login/> : (
                 <div>
-                    <h1 className="text-center pb-3">User dashboard</h1>
+                    <h1 className="text-center pb-3">Project Leader dashboard</h1>
                     <div className="mt-4">
                         <div className="container-fluid">
                             <div className="row justify-content-around">
                                 <div className="col-5 border border-primary rounded">
-                                    <h2 className="text-center p-3">My posts</h2>
+                                    <h2 className="text-center p-3">Awaiting approval stack</h2>
                                     {listItems}
                                 </div>
                                 <div className="col-5 border border-primary rounded">
-                                    <h2 className="text-center p-3">Notifications</h2>
+                                    <h2 className="text-center p-3">Awaiting assignment stack</h2>
                                 </div>
                             </div>
                         </div>
-            </div>
+                    </div>
                 </div>)}
         </div>
     )
