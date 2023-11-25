f9716be1ee0745ea03f3a1a8fb29f0a4e9b1e26f
root <root@localhost.localdomain>
2023-11-25 08:43:49 +0000

    HTTPS stuff

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import mkcert from 'vite-plugin-mkcert'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), mkcert()],
   server: {
     host: '0.0.0.0',
-    port: '80'
+    port: '443'
   },
   base: '/',
 })
