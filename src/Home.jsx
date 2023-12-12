fb572073d2301fbc72ef02ecc6fa7a159b87baef
iwl-lyam <lyam.mosnier@itwithlyam.co.uk>
2023-12-12 17:30:30 +0000

    Some style updates + favicon

--- src/Home.jsx
+++ src/Home.jsx
@@ -6,8 +6,18 @@ export default function Home() {
     <div className="text-light">
 
         <div className="">
-            <div className="bg-primary p-3 text-white d-flex justify-content-center align-items-center" style={{height:"50vh"}}>
-                <h1>All Change</h1>
+            <div className="p-3 text-white d-flex justify-content-center align-items-center" style={{height:"100vh"}}>
+                <div className={"text-center"}>
+                    <h1 style={{fontSize: 60}}>All Change</h1>
+                    <h3>Development studio making a realistic London Underground simulator on Roblox</h3>
+                </div>
+            </div>
+
+            <div className="p-4 bg-primary justify-content-center align-items-center">
+                <div className={"text-center"}>
+                    <h3>Upcoming:</h3>
+                    <h1>Project Deep Level: Jubilee</h1>
+                </div>
             </div>
         </div>
         {/*<div className="bg-secondary text-center p-3 text-white mh-100">*/}
