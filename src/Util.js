cd4363593c9f38b47e4a98de62fbf23f53ef68ef
iwl-lyam <lyam.mosnier@itwithlyam.co.uk>
2024-01-01 19:58:59 +0000

    necessary changes

--- src/Util.js
+++ src/Util.js
@@ -8,8 +8,8 @@
  */
 export const Request = async (endpoint, method="GET", body={}, auth=true) => {
     if (method === "GET") {
-        // let req = await fetch("https://allchange.xyz/"+endpoint, {
-        let req = await fetch("http://localhost:8080/"+endpoint, {
+        let req = await fetch("https://allchange.xyz/"+endpoint, {
+        // let req = await fetch("http://localhost:8080/"+endpoint, {
         headers: {
                 Authorization: (auth ? localStorage.token : "")
             }
@@ -27,8 +27,8 @@ export const Request = async (endpoint, method="GET", body={}, auth=true) => {
             console.log(err)
         }
     } else {
-        // let req = await fetch("https://allchange.xyz/"+endpoint, {
-        let req = await fetch("http://localhost:8080/"+endpoint, {
+        let req = await fetch("https://allchange.xyz/"+endpoint, {
+        // let req = await fetch("http://localhost:8080/"+endpoint, {
             headers: {
                 Authorization: (auth ? localStorage.token : ""),
                 "Content-Type": "application/json"
